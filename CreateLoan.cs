48e4979d804002059aee63e34bee72e9a24c6234
IceWolf9/11 <110422407+Icewolf911@users.noreply.github.com>
2023-08-28 21:09:47 +0200

    Update CreateLoan.cs

--- CreateLoan.cs
+++ CreateLoan.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +9,23 @@ namespace Loan_Application_System
     internal class CreateLoan : Loan
     {
         private double primeInterestRate;
-        public CreateLoan(double primeIntRate, string loanNumber, string custLastname, string custFirstname, double loanAmount, double interestRate, int term) : base(loanNumber, custLastname, custFirstname, loanAmount, interestRate, term)
+        private double loanFee;
+        private double interest;
+        private double totalAmountDue;
+        public CreateLoan(double totalAmountDue, double interest, double loanFee, double primeIntRate, string loanNumber, string custLastname, string custFirstname, double loanAmount, double interestRate, int term) : 
+            base(loanNumber, custLastname, custFirstname, loanAmount, interestRate, term)
         {
             this.PrimeInterestRate = primeIntRate;
+
+            interest = loanAmount * interestRate * term;
+            loanFee = loanAmount * loanFee;
+            totalAmountDue = loanAmount + interest + loanFee;
         }
 
         public double PrimeInterestRate { get => primeInterestRate; set => primeInterestRate = value; }
+        public double LoanFee { get => loanFee; set => loanFee = value; }
+        public double Interest { get => interest; set => interest = value; }
+        public double TotalAmountDue1 { get => totalAmountDue; set => totalAmountDue = value; }
 
         public override bool overMaxLoan(double value)
         {
@@ -25,6 +36,7 @@ namespace Loan_Application_System
             else return false;
             
         }
+        
     }
 
 }
