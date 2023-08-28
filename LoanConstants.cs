3be9ab233121997d8cd7c8a49d41d701eae8f66f
JJ <dekockjj2109@gmail.com>
2023-08-28 04:22:59 +0200

    LoanConstants interface created

---
+++ LoanConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loan_Application_System
+{
+    public interface LoanConstants
+    {
+        double shortTerm { get; }
+        double mediumTerm { get; }
+        double longTerm { get; }
+        string companyName { get; }
+        double maxLoanAmount { get; }
+    }
+}
