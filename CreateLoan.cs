4cb1e1fa0692740c866e362b03128bd59274a34e
JJ <dekockjj2109@gmail.com>
2023-08-30 18:46:27 +0200

    CreateLoan class comments complete

--- CreateLoan.cs
+++ CreateLoan.cs
@@ -6,15 +6,22 @@ using System.Threading.Tasks;
 
 namespace Loan_Application_System
 {
+    /// <summary>
+    /// inherits from Loan 
+    /// </summary>
     internal class CreateLoan : Loan
     {
         public CreateLoan(double interestRate, string custLastname, string custFirstname) : base(interestRate, custLastname, custFirstname)
         {
         }
-
+        /// <summary>
+        /// calculates the total interest due after loan term
+        /// </summary>
+        /// <returns></returns>
         protected double CalculateInterest()
         {
             double interest=0;
+            //Interest rate is devided by 100 since the input is given as a percentage
             interest = LoanAmount * InterestRate/100 * Term;
             return interest;
         }
